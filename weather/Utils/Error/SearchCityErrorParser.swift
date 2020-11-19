3dd005e60781a4cb0d9ed359aaefd95a64916747
kirill <kirill.titov@outlook.com>
2020-11-19 19:55:07 +0300

    edit add alet into errorHandler

--- weather/Utils/Error/SearchCityErrorParser.swift
+++ weather/Utils/Error/SearchCityErrorParser.swift
@@ -13,17 +13,26 @@ protocol SearchCityErrorParser: Error {
 	func parser(searchCityError: Error) -> Error
 }
 
-class ErrorHandler: SearchCityErrorParser {
+class ErrorHandler: SearchCityErrorParser, showAlert {
 	func parser(searchCityError: Error) -> Error {
 		guard let error = searchCityError.asAFError else { return searchCityError }
 		let description = "☂️⚠️ -> " + (error.errorDescription ?? "")
 		switch true {
+		case error.isResponseSerializationError:
+			showAlert(message: "Incorrect city")
+			print(description)
+		case error.isSessionTaskError:
+			showAlert(message: "No conection")
+			print(description)
 		case error.isCreateURLRequestError:
 			print(description)
-		case error.isResponseSerializationError:
+		case error.isInvalidURLError:
 			print(description)
+		case error.isCreateUploadableError:
+			print(description)
+//		case
 		default:
-			print("Unknown error")
+			print(description)
 		}
 		return error
 	}
